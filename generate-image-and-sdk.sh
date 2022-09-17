d85c9bec2c89317e6ae0cd660b4c7dee35d5f96c
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-17 12:18:06 +0900

    Update generate-image-and-sdk.sh

--- generate-image-and-sdk.sh
+++ generate-image-and-sdk.sh
@@ -289,10 +289,10 @@ echo "ASSEMBLY ${ASSEMBLY}"
 function source_backend_end() {
      if [ -z "${ND4J_HELPER}" ]; then
                       echo "Loading environment for backend ${ND4J_BACKEND}"
-                      source "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${OS}-${PLATFORM}.env"
-               elif test -f "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${OS}-${PLATFORM}${ND4J_HELPER}.env"; then
+                      source "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${ND4J_CLASSIFIER}.env"
+               elif test -f "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${ND4J_CLASSIFIER${ND4J_HELPER}.env"; then
                     echo "Loading environment for backend ${ND4J_BACKEND}"
-                    source "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${OS}-${PLATFORM}${ND4J_HELPER}.env"
+                    source "$HOME/.kompile/backend-envs/${ND4J_BACKEND}/${ND4J_CLASSIFIER}${ND4J_HELPER}.env"
                     else 
                          echo "No environment found for backend ${ND4J_BACKEND}"
                 fi
