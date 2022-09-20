dfa548a8abcaec21db3cbdf804b92b8eb92236af
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-20 17:28:31 +0900

    Update aurora-setup.sh

--- aurora-setup.sh
+++ aurora-setup.sh
@@ -13,7 +13,7 @@ yum clean expire-cache
 yum -y  group install "VE Application Runtime for Container" "VE Application Development"
 
 #VEDA/AVEO source build environment
-yum  -y -q install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve  veos-devel veos-headers libgcc-ve-static.x86_64 aurlic-lib
+yum  -y -q install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve  veos-devel veos-headers libgcc-ve-static.x86_64 aurlic-lib binutils-ve binutils-ve.src
  yum -y -q install aurlic-lib
 wget https://raw.githubusercontent.com/eclipse/deeplearning4j/master/libnd4j/build_ve_prerequisites.sh -O /kompile/build_ve_prerequisites.sh
 wget https://raw.githubusercontent.com/eclipse/deeplearning4j/master/libnd4j/vednn_mergian.patch -O /kompile/vednn_mergian.patch
