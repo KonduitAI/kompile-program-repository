8dca09d3705d301d94b0942bf1415a6923acae80
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-23 09:05:04 +0900

    Update aurora-setup.sh

--- aurora-setup.sh
+++ aurora-setup.sh
@@ -18,7 +18,7 @@ TSUBASA_GROUPS="ve-frontend nec-sdk-frontend nec-mpi-frontend"
 yum group install $TSUBASA_GROUPS
 
 #VEDA/AVEO source build environment
-yum  -y -q install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve  veos-devel veos-headers libgcc-ve-static.x86_64 aurlic-lib \
+yum  -y -q install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve  veos-devel veos-headers libgcc-ve-static.x86_64 aurlic-lib nec-nc++-shared-3.5.1.x86_64 \
 binutils-ve veoffload-aveo veosinfo.x86_64
  yum -y -q install aurlic-lib
 wget https://raw.githubusercontent.com/eclipse/deeplearning4j/master/libnd4j/build_ve_prerequisites.sh -O /kompile/build_ve_prerequisites.sh
