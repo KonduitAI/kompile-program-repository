67482bbdd1787e70b23f33ae51af69897445fe6d
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-26 13:31:19 +0900

    Update aurora-setup.sh

--- aurora-setup.sh
+++ aurora-setup.sh
@@ -16,13 +16,13 @@ yum clean expire-cache
 TSUBASA_GROUPS="ve-frontend ve-devel nec-sdk-frontend"
 /opt/nec/ve/sbin/TSUBASA-groups-remark.sh $TSUBASA_GROUPS
 yum group install $TSUBASA_GROUPS
-ln -s /opt/nec/ve/bin/nc++-3.5.1 /opt/nec/ve/bin/nc++
-ln -s /opt/nec/ve/bin/nfort-3.5.1 /opt/nec/ve/bin/nfort
-ln -s /opt/nec/ve/bin/ncc-3.5.1 /opt/nec/ve/bin/ncc
+
 
 #VEDA/AVEO source build environment 
 yum  -y install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve nec-nfort-3.5.1.x86_64 nec-nfort-shared-devel-3.5.1.x86_64 nec-nfort-shared-3.5.1.x86_64 nec-nfort-runtime.x86_64  veos-devel veos-headers nec-nc++-shared-3.5.1.x86_64    nec-nc++-shared-devel-3.5.1.x86_64 nec-veperf-devel.x86_64 nec-veperf-bin.x86_64 nec-veperf-libs.x86_64 libgcc-ve-static.x86_64 aurlic-lib nec-nc++-shared-3.5.1.x86_64 nec-nc++-3.5.1.x86_64 binutils-ve veoffload-aveo veosinfo.x86_64 aurlic-lib
-
+ln -s /opt/nec/ve/bin/nc++-3.5.1 /opt/nec/ve/bin/nc++
+ln -s /opt/nec/ve/bin/nfort-3.5.1 /opt/nec/ve/bin/nfort
+ln -s /opt/nec/ve/bin/ncc-3.5.1 /opt/nec/ve/bin/ncc
 wget https://raw.githubusercontent.com/eclipse/deeplearning4j/master/libnd4j/build_ve_prerequisites.sh -O /kompile/build_ve_prerequisites.sh
 wget https://raw.githubusercontent.com/eclipse/deeplearning4j/master/libnd4j/vednn_mergian.patch -O /kompile/vednn_mergian.patch
 sed -i 's|sudo||' /kompile/build_ve_prerequisites.sh
