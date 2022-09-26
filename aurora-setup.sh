8bdb3ce6dc878cae9683c015f37e5a713eb2719d
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-26 13:21:24 +0900

    Update aurora-setup.sh

--- aurora-setup.sh
+++ aurora-setup.sh
@@ -19,7 +19,6 @@ yum group install $TSUBASA_GROUPS
 ln -s /opt/nec/ve/bin/nc++-3.5.1 /opt/nec/ve/bin/nc++
 ln -s /opt/nec/ve/bin/nfort-3.5.1 /opt/nec/ve/bin/nfort
 ln -s /opt/nec/ve/bin/ncc-3.5.1 /opt/nec/ve/bin/ncc
-ln -s $HOME/.kompile/python/bin/python /usr/bin/python
 
 #VEDA/AVEO source build environment 
 yum  -y install  systemd-devel libsysve-devel.x86_64 glibc-ve-devel kheaders-ve nec-nfort-3.5.1.x86_64 nec-nfort-shared-devel-3.5.1.x86_64 nec-nfort-shared-3.5.1.x86_64 nec-nfort-runtime.x86_64  veos-devel veos-headers nec-nc++-shared-3.5.1.x86_64    nec-nc++-shared-devel-3.5.1.x86_64 nec-veperf-devel.x86_64 nec-veperf-bin.x86_64 nec-veperf-libs.x86_64 libgcc-ve-static.x86_64 aurlic-lib nec-nc++-shared-3.5.1.x86_64 nec-nc++-3.5.1.x86_64 binutils-ve veoffload-aveo veosinfo.x86_64 aurlic-lib
