72c2492b8476a1d3d5d17d54066d534f59aa4ffe
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-09-28 09:48:43 +0900

    Update generate-image-and-sdk.sh

--- generate-image-and-sdk.sh
+++ generate-image-and-sdk.sh
@@ -323,7 +323,7 @@ if [ ! -z "$GCC" -a "$GCC" != "" ]; then
         ./kompile install install-tool --programName="${GCC}"
         export CC="$HOME/.kompile/${GCC}/bin/gcc"
         export CXX="$HOME/.kompile/${GCC}/bin/g++"
-        if [ -z "$LD_LIBRARY_PATH" ]; then
+        if [ -z "${LD_LIBRARY_PATH-}" ]; then
               export LD_LIBRARY_PATH="$HOME/.kompile/${GCC}/lib64" 
            else
              export LD_LIBRARY_PATH="$HOME/.kompile/${GCC}/lib64:$LD_LIBRARY_PATH"
@@ -336,7 +336,7 @@ if [ ! -z "$GLIBC" -a "$GLIBC" != "" ]; then
         echo "Setting custom GLIBC"
         ./kompile install install-tool --programName="${GLIBC}"
         export PATH="$HOME/.kompile/${GLIBC}/bin:$PATH"
-        if [ -z "$LD_LIBRARY_PATH" ]; then
+        if [ -z "${LD_LIBRARY_PATH-}" ]; then
               export LD_LIBRARY_PATH="$HOME/.kompile/${GLIBC}/lib64" 
        else
              export LD_LIBRARY_PATH="$HOME/.kompile/${GLIBC}/lib64:$LD_LIBRARY_PATH"
